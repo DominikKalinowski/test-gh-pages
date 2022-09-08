d2b4ed6f9ba31cf42e131367e66f00a2b4b9e732
Dogson <myszkowski.lukasz01@gmail.com>
2022-09-08 17:30:26 +0200

    refactor: delete nxWelcomeComponent

--- apps/cook-it/src/app/app.module.ts
+++ apps/cook-it/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidenavComponentModule } from '@cook-it/feature-recipe-list';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -15,7 +14,7 @@ const environment = {
 };
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
